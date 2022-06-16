741f8a119aeefe9bfa5374736a9c30cfecde8780
Christina Rontell <clrontell@gmail.com>
2022-06-15 23:10:31 -0500

    button toggle campsite

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
