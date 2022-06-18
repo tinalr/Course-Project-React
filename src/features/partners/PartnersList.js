d98295b242c08c2b27d36504f74b88d3249c38ed
Christina Rontell <clrontell@gmail.com>
2022-06-18 12:10:28 -0500

    AboutPage

---
+++ src/features/partners/PartnersList.js
@@ -0,0 +1,22 @@
+import { Row, Col } from "reactstrap";
+import Partner from './Partner';
+import { selectAllPartners } from './partnersSlice';
+
+const PartnersList = () => {
+    const partners = selectAllPartners();
+
+    return (
+        <Col className="mt-4">
+            <Row>
+                {partners.map((partner) => {
+                    return (
+                        <div className="d-flex mb-5" key={partner.id}>
+                            <Partner partner={partner} />
+                        </div>
+                    );
+                })}
+            </Row>
+        </Col>
+    );
+};
+export default PartnersList;
