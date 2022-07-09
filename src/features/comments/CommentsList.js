e58f90571045d13cc2abad5b270c238073c7c5de
Christina Rontell <clrontell@gmail.com>
2022-07-09 11:14:01 -0500

    Workshop Tasks 1 & 2

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -3,12 +3,19 @@ import Comment from "./Comment";
 import { selectCommentsByCampsiteId } from "./commentsSlice";
 import CommentForm from "./CommentForm";
 import { useSelector } from "react-redux";
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 
 const CommentsList = ({ campsiteId }) => {
     const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
+    const isLoading = useSelector((state) => state.comments.isLoading);
+    const errMsg = useSelector((state) => state.comments.errMsg);
 
-    if (comments && comments.length > 0) {
-        return (
+    return isLoading ? (
+        <Loading />
+    ) : errMsg ? (
+        <Error errMsg={errMsg} />
+    ) : comments && comments.length > 0 ? (
             <Col md='5' className="m-1">
                 <h4>Comments</h4>
                 {comments.map((comment) => {
@@ -16,13 +23,28 @@ const CommentsList = ({ campsiteId }) => {
                 })}
                 <CommentForm campsiteId={campsiteId}></CommentForm>
             </Col>
+        ) : (
+            <Col md='5' className="m-1">
+                There are no comments for this campsite yet.
+            </Col>
         );
-    }
-    return (
-        <Col md='5' className="m-1">
-            There are no comments for this campsite yet.
-        </Col>
-    );
+
+    // if (comments && comments.length > 0) {
+    //     return (
+    //         <Col md='5' className="m-1">
+    //             <h4>Comments</h4>
+    //             {comments.map((comment) => {
+    //                 return <Comment key={comment.id} comment={comment} />;
+    //             })}
+    //             <CommentForm campsiteId={campsiteId}></CommentForm>
+    //         </Col>
+    //     );
+    // }
+    // return (
+    //     <Col md='5' className="m-1">
+    //         There are no comments for this campsite yet.
+    //     </Col>
+    // );
 };
 
 export default CommentsList;
